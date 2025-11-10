2c3fd094ad860b25cb01047cb3d858ba0de1b2d0
c <wellnessvoid@gmail.com>
2025-11-10 00:16:21 -0500

    added front part to cadquery model

--- cad/cq/pipe_parts.py
+++ cad/cq/pipe_parts.py
@@ -63,12 +63,14 @@ def pipe_side(
 ):
     total_height = pipe_length + foot_cavity_height
     part = (
+        # stock
         cq.Workplane("XY")
         .rect(
             inner_width+2*dado_depth,
             total_height,
         )
         .extrude(stock_thickness)
+        # back dado groove
         .faces(">Z")
         .workplane(
             origin=(
@@ -79,15 +81,50 @@ def pipe_side(
         )
         .rect(dado_depth, total_height)
         .cutBlind(-(stock_thickness-dado_width))
+        # front dado groove
         .faces(">Z")
         .workplane(
             origin=(
                 (1 if not right_side else -1) * (inner_width+dado_depth)/2,
-                foot_cavity_height+upper_lip_pipe_length,
+                (foot_cavity_height+upper_lip_pipe_length)/2,
                 0)
         )
         .rect(dado_depth, total_height-upper_lip_pipe_length-foot_cavity_height)
         .cutBlind(-(stock_thickness-dado_width))
+    )
+    if show:
+        show_object(part, options={"alpha":0.5, "color": (1.0, 1.0, 1.0)})
+    return part
+
+def pipe_front(
+        stock_thickness=Defaults.stock_thickness,
+        inner_width=Defaults.inner_width,
+        pipe_length=Defaults.pipe_length,
+        upper_lip_pipe_length=Defaults.upper_lip_pipe_length,
+        foot_cavity_height=Defaults.foot_cavity_height,
+        dado_width=Defaults.dado_width,
+        dado_depth=Defaults.dado_depth,
+        show=False,
+):
+    total_height = pipe_length + foot_cavity_height
+    part = (
+        # stock
+        cq.Workplane("XY")
+        .rect(
+            inner_width+2*stock_thickness,
+            pipe_length-upper_lip_pipe_length,
+        )
+        .extrude(stock_thickness)
+        # left dado groove
+        .faces(">Z")
+        .workplane(origin=((inner_width+dado_width)/2,0,0))
+        .rect(dado_width, pipe_length-upper_lip_pipe_length)
+        .cutBlind(-dado_depth)
+        # right dado groove
+        .faces(">Z")
+        .workplane(origin=(-(inner_width+dado_width)/2,0,0))
+        .rect(dado_width, pipe_length-upper_lip_pipe_length)
+        .cutBlind(-dado_depth)
     )
     if show:
         show_object(part, options={"alpha":0.5, "color": (1.0, 1.0, 1.0)})
@@ -135,6 +172,16 @@ def generate(
         right_side=False
     )
 
+    front = pipe_front(
+        stock_thickness=stock_thickness,
+        inner_width=inner_width,
+        pipe_length=pipe_length,
+        upper_lip_pipe_length=upper_lip_pipe_length,
+        foot_cavity_height=foot_cavity_height,
+        dado_width=dado_width,
+        dado_depth=dado_depth
+    )
+
     if show_assembly:
         assembly = cq.Assembly()
         assembly.add(back,
@@ -163,6 +210,18 @@ def generate(
                 0, -90, 0
             )
         )
+        assembly.add(
+            front,
+            name="=front",
+            color=cq.Color(0.44, 0.94, 0.4, 0.3),
+            loc=cq.Location(
+                0,
+                upper_lip_pipe_length,
+                stock_thickness*2 + inner_width + exploded_by*2,
+                0, 180, 0
+            )
+        )
         show_object(assembly)
 
-generate(show_assembly=True, exploded_by=10)
+generate(show_assembly=True, exploded_by=0)
+# pipe_front(show=True)
